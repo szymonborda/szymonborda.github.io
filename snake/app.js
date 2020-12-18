b559e71a5d2810dacac99e9de3925133fa67a874
Szymon Borda <szbk20@gmail.com>
2020-12-18 17:27:35 +0100

    Add files via upload

--- snake/app.js
+++ snake/app.js
@@ -1,154 +1,195 @@
-let snake = [
-    {
-        x: 11,
-        y: 11
-    }
-]
-
-let apple = {
-    x: 2,
-    y: 8
-}
-
-let snakeDirection = {
-    x: 0,
-    y: -1
-}
-
-let lastClickedKey = 0
-
-const game = document.getElementById('game')
-
-let render = () => {
-    game.innerHTML = ''
-    snake.forEach((part) => {
-        let snakeDiv = document.createElement('div')
-        snakeDiv.style.gridRowStart = part.y
-        snakeDiv.style.gridColumnStart = part.x
-        snakeDiv.classList.add('snake')
-        game.appendChild(snakeDiv)
-    })
-
-    let appleDiv = document.createElement('div')
-    appleDiv.style.gridRowStart = apple.y
-    appleDiv.style.gridColumnStart = apple.x
-    appleDiv.classList.add('apple')
-    game.appendChild(appleDiv)
-}
-
-let placeApple = () => {
-    apple.x = Math.floor(Math.random() * 21) + 1
-    apple.y = Math.floor(Math.random() * 21) + 1
-    snake.forEach(part => {
-        if ((part.x == apple.x) && (part.y == apple.y)) {
-            placeApple()
+class Snake {
+    constructor() {
+        this.parts = [
+            {
+                x: 11,
+                y: 11
+            }
+        ]
+        this.direction = {
+            x: 0,
+            y: -1
         }
-    })
-}
-
-let gameTick = () => {
-    for (let i = snake.length - 1; i >= 0; i--) {
-        if (i == 0) {
-            snake[i].x += snakeDirection.x
-            snake[i].y += snakeDirection.y
-        } else {
-            snake[i].x = snake[i - 1].x
-            snake[i].y = snake[i - 1].y
+        this.lastPartLastTick = {
+            x: this.parts[this.parts.length - 1].x,
+            y: this.parts[this.parts.length - 1].y
         }
     }
-}
 
-let keyTriggers = () => {
-    document.addEventListener('keydown', (event) => {
-        lastClickedKey = event.keyCode
-    })
-}
-
-let changeDirection = () => {
-    switch(lastClickedKey) {
-        case 37:
-        case 65:
-            if(snakeDirection.x != 1) {
-                snakeDirection = {
-                    x: -1,
-                    y: 0
-                }
+    move() {
+        this.lastPartLastTick.x = this.parts[this.parts.length - 1].x
+        this.lastPartLastTick.y = this.parts[this.parts.length - 1].y
+        for (let i = this.parts.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                this.parts[i].x += this.direction.x
+                this.parts[i].y += this.direction.y
+            } else {
+                this.parts[i].x = this.parts[i - 1].x
+                this.parts[i].y = this.parts[i - 1].y
             }
-            break;
-        case 38:
-        case 87:
-            if(snakeDirection.y != 1) {
-                snakeDirection = {
-                    x: 0,
-                    y: -1
-                }
-            }
-            break;
-        case 39:
-        case 68:
-            if(snakeDirection.x != -1) {
-                snakeDirection = {
-                    x: 1,
-                    y: 0
-                }
-            }
-            break;
-        case 40:
-        case 83:
-            if(snakeDirection.y != -1) {
-                snakeDirection = {
-                    x: 0,
-                    y: 1
-                }
-            }
-            break;   
-    }
-}
-
-let isGameOver = () => {
-    if ((snake[0].x > 21) || (snake[0].y > 21) || (snake[0].x < 1) || (snake[0].y < 1)) {
-        console.log('ściana');
-        return true
+        }
     }
 
-    for (let i = 1; i < snake.length; i++) {
-        if ((snake[i].x == snake[0].x) && (snake[i].y == snake[0].y)) {
-            console.log('kanibal');
+    grow() {
+        this.parts.push({
+            x: this.lastPartLastTick.x,
+            y: this.lastPartLastTick.y
+        })
+    }
+
+    changeDirection(lastClickedKey) {
+        switch(lastClickedKey) {
+            case 37:
+            case 65:
+                if(this.direction.x != 1) {
+                    this.direction = {
+                        x: -1,
+                        y: 0
+                    }
+                }
+                break;
+            case 38:
+            case 87:
+                if(this.direction.y != 1) {
+                    this.direction = {
+                        x: 0,
+                        y: -1
+                    }
+                }
+                break;
+            case 39:
+            case 68:
+                if(this.direction.x != -1) {
+                    this.direction = {
+                        x: 1,
+                        y: 0
+                    }
+                }
+                break;
+            case 40:
+            case 83:
+                if(this.direction.y != -1) {
+                    this.direction = {
+                        x: 0,
+                        y: 1
+                    }
+                }
+                break;   
+        }
+    }
+
+    isDead() {
+        if ((this.parts[0].x > 21) || (this.parts[0].y > 21) || (this.parts[0].x < 1) || (this.parts[0].y < 1)) {
+            console.log('ściana');
             return true
         }
-    }
-
-    return false
-}
-
-let isAppleEaten = () => {
-    if ((snake[0].x == apple.x) && (snake[0].y == apple.y)) {
-        if (snake.length > 1) {
-            snake.push({
-                x: snake[snake.length - 1].x - (snake[snake.length - 1].x - snake[snake.length - 2].x),
-                y: snake[snake.length - 1].y - (snake[snake.length - 1].y - snake[snake.length - 2].y)
-            })
-        } else {
-            snake.push({
-                x: snake[snake.length - 1].x - snakeDirection.x,
-                y: snake[snake.length - 1].y - snakeDirection.y
-            })
+    
+        for (let i = 1; i < this.parts.length; i++) {
+            if ((this.parts[i].x == this.parts[0].x) && (this.parts[i].y == this.parts[0].y)) {
+                console.log('kanibal');
+                return true
+            }
         }
-        
-        placeApple()
+    
+        return false
     }
 }
 
-keyTriggers()
-placeApple()
+class Apple {
+    constructor(snake) {
+        this.pos = {
+            x: 1,
+            y: 1
+        }
+        this.newPos(snake)
+    }
+
+    newPos(snake) {
+        this.pos.x = Math.floor(Math.random() * 21) + 1
+        this.pos.y = Math.floor(Math.random() * 21) + 1
+        snake.parts.forEach(part => {
+            if ((part.x == this.pos.x) && (part.y == this.pos.y)) {
+                this.newPos(snake)
+            }
+        })
+    }
+
+    isEaten(snake) {
+        if ((snake.parts[0].x == this.pos.x) && (snake.parts[0].y == this.pos.y)) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Game {
+    constructor(board) {
+        this.board = board
+        this.lastClickedKey = 0
+        this.keyTriggers()
+    }
+
+    render(snake, apple) {
+        //this.board.innerHTML = ''
+        document.querySelectorAll('.snake').forEach((elem) => {
+            elem.remove()
+        })
+        document.querySelectorAll('.apple').forEach((elem) => {
+            elem.remove()
+        })
+        snake.parts.forEach((part) => {
+            let snakeDiv = document.createElement('div')
+            snakeDiv.style.gridColumnStart = part.x
+            snakeDiv.style.gridRowStart = part.y
+            snakeDiv.classList.add('snake')
+            this.board.appendChild(snakeDiv)
+        })
+
+        let appleDiv = document.createElement('div')
+        appleDiv.style.gridColumnStart = apple.pos.x
+        appleDiv.style.gridRowStart = apple.pos.y
+        appleDiv.classList.add('apple')
+        this.board.appendChild(appleDiv)
+    }
+
+    keyTriggers() {
+        document.addEventListener('keydown', (event) => {
+            this.lastClickedKey = event.keyCode
+        })
+
+        document.getElementById('move-left').addEventListener('click', () => {
+            this.lastClickedKey = 37
+        })
+
+        document.getElementById('move-up').addEventListener('click', () => {
+            this.lastClickedKey = 38
+        })
+
+        document.getElementById('move-right').addEventListener('click', () => {
+            this.lastClickedKey = 39
+        })
+
+        document.getElementById('move-down').addEventListener('click', () => {
+            this.lastClickedKey = 40
+        })
+    }
+}
+
+let snake1 = new Snake()
+let apple1 = new Apple(snake1)
+let game = new Game(document.getElementById('game'))
 
 let gameInterval = setInterval(() => {
-    changeDirection()
-    gameTick()
-    if (isGameOver()) {
+    snake1.changeDirection(game.lastClickedKey)
+    snake1.move()
+    if (snake1.isDead()) {
         clearInterval(gameInterval)
         return
     }
-    isAppleEaten()
-    render()
+    if (apple1.isEaten(snake1)) {
+        snake1.grow()
+        apple1.newPos(snake1)
+    }
+    game.render(snake1, apple1)
 }, 200);
